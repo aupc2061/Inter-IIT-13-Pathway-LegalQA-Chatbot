ecdcd6cc3573cf657c1e81461562d470940d913e
himanshu-skid19 <114365148+himanshu-skid19@users.noreply.github.com>
2024-12-18 13:51:03 +0530

    Update globals_.py

--- legal-chatbot-frontend/src/final_scripts/globals_.py
+++ legal-chatbot-frontend/src/final_scripts/globals_.py
@@ -3,4 +3,4 @@ bad_docs = set()
 counter_ =0
 LANGFUSE_HOST="https://cloud.langfuse.com"
 THRESHOLD=25
-MONGO_URL ="mongodb+srv://himanshusinghal2003:32usfAJfjiWToy5r@legal.4rqce.mongodb.net/?retryWrites=true&w=majority&appName=legal"
+MONGO_URL ="insert momgo url here"
